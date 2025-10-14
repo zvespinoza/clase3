689062db202dbc7597bd70e26f48d17686f0e6df
zvespinoza <zvespinoza@istlatroncal.edu.ec>
2025-10-14 18:22:09 -0500

    listas de asignaturas

--- clase3/#2 16julio.py
+++ clase3/#2 16julio.py
@@ -1,6 +1,8 @@
-contraseña=123456
-clave = int(input("ingrese su contraseña: "))
-if contraseña == clave:
-    print("acesso concedido")
-else:
-    print("acesso denegado")
+#contraseña=123456
+#clave = int(input("ingrese su contraseña: "))
+#if contraseña == clave:
+    #print("acesso concedido")
+#else:
+    #print("acesso denegado")
+
+ciclo = ("POO", "DISEÑO", "BDATOS2", "MDS", "MA2")
